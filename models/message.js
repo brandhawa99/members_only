34a886484421141b3305d1d5da73e385346d23a2
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-11 16:15:25 -0700

    changed max length of title

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ var {DateTime} = require('luxon');
 var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema({
-  title:{type:String, required:true, minlength:1, maxlength:30},
+  title:{type:String, required:true, minlength:1, maxlength:45},
   message:{type:String, required:true, maxlength:1500},
   timestamp: {type:Date, required:true, default:Date.now()},
   user: {type: Schema.Types.ObjectId, ref:"User", required:true}
