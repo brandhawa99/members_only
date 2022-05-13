81e0957979e56d78d133c156a99edf40e32e4129
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-13 14:41:25 -0700

    added routes for sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const user_controller = require('../controllers/UserController')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', user_controller.index);
+
+router.get('/sign-up',user_controller.user_signup_get)
+
+router.post('/sign-up',user_controller.user_signup_post)
+
+router.get('/login', user_controller.user_login_get);
 
 module.exports = router;
