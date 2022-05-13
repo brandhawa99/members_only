d0d8e96ef1f96b9901cf36c03c07b9f48bd243a6
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-13 14:41:36 -0700

    asdf

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+var session = require('express-session');
+const User = require('./models/user')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const mongodb = process.env.MONGO_URI
+mongoose.connect(mongodb,{useUnifiedTopology:true,useNewUrlParser:true})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"mongo connection error"));
 
 var app = express();
 
@@ -13,14 +23,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+passport.use(new LocalStrategy(function verify (username,password, cb){
+  User.findOne({'username': username}, (err,user)=>{
+    if(err){
+      return cb(err);
+    }
+    if(!user){
+      return cb(null,false,{message:"Incorrect username"})
+    }
+    if(user.password !== password){
+      return cb(null,false,{message:"Incorrect Password"})
+    }
+    return cb(null,user);
+  })
+}))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
