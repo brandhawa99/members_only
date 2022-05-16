89da79386d4af48aecce3fcccee71c9755b0bc2e
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-15 20:22:38 -0700

    create get and post for messages

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,6 +1,29 @@
-
+const Message = require('../models/message');
 
 
 exports.message_get = function(req,res,next){
-  res.render('')
+  if(req.user=== undefined){
+    res.redirect('/log-in')
+  }
+  res.render('message_form',{title:'Create A Post', user:req.user})
 }
+
+exports.message_post = [
+
+
+  (req,res,next) =>{
+    let message = new Message({
+      title: req.body.title,
+      message: req.body.message,
+      user: req.body.userid
+    });
+    message.save(function(err){
+      if(err){return next(err)}
+      console.log(req.user._id)
+      res.redirect('/home');
+
+    })
+
+  }
+
+]
